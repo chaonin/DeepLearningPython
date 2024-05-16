1f29e90cbe6793ad13235c482acada1a89f1117b
Chaonian Guo <chaonin@gmail.com>
2024-05-16 14:50:48 +0800

    update

--- gen-neural-net.py
+++ gen-neural-net.py
@@ -14,28 +14,29 @@ fig, ax = plt.subplots(figsize=(10, 6))
 # 绘制输入层节点
 for i in range(input_layer):
     ax.scatter(0, 1 - i * vertical_distance, color='blue', s=300)
-    ax.text(0, 1 - i * vertical_distance, f'Input {i+1}', ha='right', va='center')
+    ax.text(0, 1 - i * vertical_distance, f'Input {i + 1}', ha='right', va='center')
 
 # 绘制中间层节点
 for i in range(hidden_layer):
     ax.scatter(0.5, 1 - i * vertical_distance, color='green', s=300)
-    ax.text(0.5, 1 - i * vertical_distance, f'Hidden {i+1}', ha='right', va='center')
+    ax.text(0.5, 1 - i * vertical_distance, f'Hidden {i + 1}', ha='right', va='center')
 
 # 绘制输出层节点
 for i in range(output_layer):
     ax.scatter(1, 1 - i * vertical_distance, color='red', s=300)
-    ax.text(1, 1 - i * vertical_distance, f'Output {i+1}', ha='left', va='center')
+    ax.text(1, 1 - i * vertical_distance, f'Output {i + 1}', ha='left', va='center')
 
 # 绘制连接线
 for i in range(input_layer):
     for j in range(hidden_layer):
-        ax.plot([0, 0.5], [1 - i * vertical_distance, 1 - j * vertical_distance], color='black')
+        ax.plot([0, 0.5], [1 - i * vertical_distance, 1 - j * vertical_distance], color='black', linewidth=0.5)
 
 for i in range(hidden_layer):
     for j in range(output_layer):
-        ax.plot([0.5, 1], [1 - i * vertical_distance, 1 - j * vertical_distance], color='black')
+        ax.plot([0.5, 1], [1 - i * vertical_distance, 1 - j * vertical_distance], color='black', linewidth=0.5)
 
 # 隐藏坐标轴
 ax.axis('off')
 
 plt.show()
+
