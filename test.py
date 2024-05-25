38c53b93aea15ef8c386aaee9579b52bd283150d
Dream3 <dream3@Dream3s-Mac-Studio.local>
2024-05-25 22:24:05 +0800

    update

--- test.py
+++ test.py
@@ -208,4 +208,3 @@ net = Network([
     FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
-
