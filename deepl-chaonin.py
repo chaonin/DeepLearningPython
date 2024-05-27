97c25608dfc0af1d8fe9312eca3ca4a2499925c9
chaonin <chaonin@gmail.com>
2024-05-27 20:03:21 +0800

    update

--- deepl-chaonin.py
+++ deepl-chaonin.py
@@ -1,6 +1,11 @@
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
+for d in test_data:
+    print(d)
+    break 
+"""
 import network
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+"""
